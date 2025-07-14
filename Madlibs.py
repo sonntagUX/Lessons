2c17fed68c5b7b27796c232c7b5da34c1daa85a3
sonntagUX <dedesonntag@gmail.com>
2025-07-14 11:58:48 -0700

    Update Madlibs.py

--- Madlibs.py
+++ Madlibs.py
@@ -1,7 +1,10 @@
-import streamlit
+import streamlit as st
 
-color = input("Enter a color")
-plural_noun = input("Enter a plural noun")
-celebrity = input("Enter a celebrity's name")
+color = st.text_input("Enter a color")
+plural_noun = st.text_input("Enter a plural noun")
+celebrity = st.text_input("Enter a celebrity's name")
 
-
+if color and plural_noun and celebrity:
+    st.write(f"Roses are {color}")
+    st.write(f"{plural_noun} are blue")
+    st.write(f"I love {celebrity}")
