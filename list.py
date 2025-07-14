285e987f973da972a967db9f02243ad684f7873f
sonntagUX <dedesonntag@gmail.com>
2025-07-14 12:22:29 -0700

    Update list.py

--- list.py
+++ list.py
@@ -1,6 +1,9 @@
 import streamlit as st
-lucky_numbers = [7, 8, 9, 166]
+lucky_numbers = [70, 18, 9, 166]
 names = ["Jim", "Jim", "Joe", "Jack", "Jack", "Jack"]
 
 names.sort()
 st.write(names)
+
+names.sort()
+st.write(lucky_numbers)
